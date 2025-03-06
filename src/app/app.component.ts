19d4910b8d2b1ae1e4de0a78ca92998c7d0f18ff
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-03-06 10:32:38 +0530

    [Hritik] adding some changes in app.ts and app.html and app.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,  // Marking the component as standalone
+  imports: [RouterOutlet, FormsModule],  // Import necessary modules here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HelloWorld';
+  title = 'Helloworld';
+  imgUrl = "/logo.jpeg";
+  userName: string = "";
+  nameError:string="";
+  url = "https://www.bridgelabz.com";
+
+  ngOnInit(): void {
+    this.title = "Hello from BridgeLabz.";
+  }
+
+  onClick($event: MouseEvent): void {
+    console.log("Save button is clicked", $event);
+    window.open(this.url, "_blank");
+  }
+
+  onInput($event:any){
+    console.log("Change event occurred",$event.data);
+    const nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect";
+  }
 }
